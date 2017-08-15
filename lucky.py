8414e311c9053dce6d50955620c87b6285c8e29d
yangLi <yang.li@anchora.me>
2017-08-15 16:09:23 +0800

    update upload images

--- lucky.py
+++ lucky.py
@@ -1,23 +1,51 @@
 from flask import Flask, render_template
 import pymysql
+from flask_uploads import UploadSet, IMAGES, configure_uploads, ALL
+from flask import request, Flask, redirect, url_for, render_template
+import os
 
-# db = pymysql.connect("127.0.0.1", "root", "root", "flight_tickets", charset='utf8')
+db = pymysql.connect("127.0.0.1", "root", "root", "lucky", charset='utf8')
 
 app = Flask(__name__)
+app.config['UPLOADED_PHOTO_DEST'] = os.path.dirname(os.path.abspath(__file__)) + "/static/images/"
+app.config['UPLOADED_PHOTO_ALLOW'] = IMAGES
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    return os.path.dirname(os.path.abspath(__file__)) + "/static/images/"
 
 
 @app.route('/lucky', methods=['GET', 'POST'])
 def lucky():
-    # cursor = db.cursor()
-    # sql = "SELECT distinct airline_name FROM flight where price > 2000"
-    # cursor.execute(sql)
+    cursor = db.cursor()
+    sql = "SELECT name FROM images"
+    cursor.execute(sql)
     # results = cursor.fetchall()
-    return render_template('lucky.html')
+    test = 'test'
+    return render_template('lucky.html', test=test)
+
+
+photos = UploadSet('PHOTO')
+configure_uploads(app, photos)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_img():
+    path = os.path.dirname(os.path.abspath(__file__)) + "/static/images/"
+    if request.method == 'POST':
+        uploaded_files = request.files.getlist("photo[]")
+        for img in uploaded_files:
+            photos.save(img)
+            cursor = db.cursor()
+            sql = "INSERT INTO images(name, path) values (%s, %s)"
+            cursor.execute(sql, (img.filename, path+img.filename))
+            db.commit()
+        db.close()
+        return "ttt"
+        # return redirect(url_for('lucky', name=filename))
+    return render_template('upload_img.html')
+
 
 if __name__ == '__main__':
     app.run()
